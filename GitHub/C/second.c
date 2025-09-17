c0c76aaf0cfc9c753f66fde448793e5a9380bb23
Celso Herib Ortigoza Duarte <102439750+CelsoHerib@users.noreply.github.com>
2025-09-17 18:30:37 -0300

    creacion de capeta de programa C

---
+++ GitHub/C/second.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+  printf("Hello World!\n");
+  printf("I am learning C.\n");
+  printf("And it is awesome!\n");
+  printf("And this is a longer text.\nBut is written in a single line.\n");
+  printf("And in this paragraph we will see something interesting.\t");
+  printf("Creates a horizontal tab\n");
+  printf("They call him \"John Doe\"."); //This is a single comment
+  return 0;
+}
