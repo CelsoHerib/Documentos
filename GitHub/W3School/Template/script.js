012bf711fefffcf910e666d76eb851d07d7737d3
Celso Herib Ortigoza Duarte <102439750+CelsoHerib@users.noreply.github.com>
2025-09-23 09:24:16 -0300

    creacion de varios elementos carpetas archivos etc

---
+++ GitHub/W3School/Template/script.js
@@ -0,0 +1,25 @@
+const expandBtn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expandBtn.addEventListener("click", () => {
+    document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+const allLinks = document.querySelectorAll(".sidebar-links a");
+
+allLinks.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((link) => {
+            if (link.href === hrefLinkClick) {
+                link.classList.add("active");
+            }
+            else {
+                link.classList.remove("active");
+            }
+        });
+    });
+});
