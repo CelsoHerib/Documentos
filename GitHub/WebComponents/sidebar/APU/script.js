cef2ff05b26e3ca48661269c24c385ec5a3459cd
Celso Herib Ortigoza Duarte <102439750+CelsoHerib@users.noreply.github.com>
2025-09-24 19:54:03 -0300

    creacion de modo oscuro y usuario

--- GitHub/WebComponents/sidebar/APU/script.js
+++ GitHub/WebComponents/sidebar/APU/script.js
@@ -1,7 +1,15 @@
 const bug = document.getElementById('bug');
 const barraLateral = document.querySelector('.barra-lateral');
 const spans = document.querySelectorAll('span');
+const interruptor = document.querySelector('.interruptor');
+const circulo = document.querySelector('.circulo');
 
+
+interruptor.addEventListener("click", () => {
+    let body = document.body;
+    body.classList.toggle('tema-oscuro');
+    circulo.classList.toggle('prendido');
+});
 
 bug.addEventListener("click", () => {
     barraLateral.classList.toggle('mini-barra-lateral');
