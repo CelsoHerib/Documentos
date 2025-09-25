22f6fe6d07aed183a93aa068eed8363992618b29
Celso Herib Ortigoza Duarte <102439750+CelsoHerib@users.noreply.github.com>
2025-09-24 21:41:43 -0300

    completado

--- GitHub/WebComponents/sidebar/APU/script.js
+++ GitHub/WebComponents/sidebar/APU/script.js
@@ -3,7 +3,28 @@ const barraLateral = document.querySelector('.barra-lateral');
 const spans = document.querySelectorAll('span');
 const interruptor = document.querySelector('.interruptor');
 const circulo = document.querySelector('.circulo');
+const menu = document.querySelector('.menu');
+const main = document.querySelector('main');
 
+
+
+menu.addEventListener("click", () => {
+    barraLateral.classList.toggle('max-barra-lateral');
+    if(barraLateral.classList.contains("max-barra-lateral")){
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block";
+    }else{
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";
+    }
+    if(window.innerWidth<=320){
+        barraLateral.classList.add('mini-barra-lateral');
+        main.classList.add('mini-main');
+        spans.forEach((span) => {
+            span.classList.add('oculto');
+        });
+    }
+});
 
 interruptor.addEventListener("click", () => {
     let body = document.body;
@@ -13,6 +34,7 @@ interruptor.addEventListener("click", () => {
 
 bug.addEventListener("click", () => {
     barraLateral.classList.toggle('mini-barra-lateral');
+    main.classList.toggle('mini-main');
     spans.forEach((span) => {
         span.classList.toggle('oculto');
     });
